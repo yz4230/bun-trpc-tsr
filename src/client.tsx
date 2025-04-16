9e0e80c1300e20ac0d3746df3ffe608987e73ec5
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2025-04-17 03:30:21 +0900

    chore: reorganize imports

--- src/client.tsx
+++ src/client.tsx
@@ -1,13 +1,10 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-
-// Import the generated route tree
-import { routeTree } from "./routeTree.gen.ts";
-
-import { QueryClientProvider } from "@tanstack/react-query";
 import reportWebVitals from "./client/reportWebVitals.ts";
 import { queryClient } from "./client/tanstack-query.ts";
+import { routeTree } from "./routeTree.gen.ts";
 import "./styles.css";
 
 // Create a new router instance
