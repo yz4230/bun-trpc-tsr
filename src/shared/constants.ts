59e40bd5e34d847ef4d11be454e828e25260d6da
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2025-04-16 22:07:07 +0900

    fix: handle undefined NODE_ENV in IS_DEV constant

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,3 +1,3 @@
-export const IS_DEV = process.env.NODE_ENV === "development";
+export const IS_DEV = (process.env.NODE_ENV ?? "development") === "development";
 export const DEFAULT_SERVER_PORT = IS_DEV ? 3001 : 3000;
 export const TRPC_ENDPOINT = "/trpc";
